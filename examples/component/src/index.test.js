eb5c73f1e070eff452388e6d55694e747155cc69
Jamal Carvalho <jamal.a.carvalho@gmail.com>
2018-11-25 09:28:43 -0500

    add vaporweb component example

---
+++ examples/component/src/index.test.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import MyComponent from './index'
+
+describe('<MyComponent />', () => {
+  test('renders without exploding', () => {
+    const div = document.createElement('div')
+    ReactDOM.render(<MyComponent />, div)
+  })
+})
