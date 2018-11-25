a4f0cc7fe722e1e66b6745a7a96a55fbd65c9e9d
Jamal Carvalho <jamal.a.carvalho@gmail.com>
2018-11-25 13:55:42 -0500

    fix deps, add typescript example

---
+++ examples/component-typescript/src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+interface Props {
+
+}
+
+export default class MyComponent extends React.Component<Props> {
+  render() {
+    return <div>Ｈｅｌｌｏ，　Ｗｏｒｌｄ！　こんにちは</div>
+  }
+}
