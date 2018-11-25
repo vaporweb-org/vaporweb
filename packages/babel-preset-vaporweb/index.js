0686d7a49082eba4a84554e39943c3d8bad432e2
Jamal Carvalho <jamal.a.carvalho@gmail.com>
2018-11-25 14:46:56 -0500

    tsconfig

--- packages/babel-preset-vaporweb/index.js
+++ packages/babel-preset-vaporweb/index.js
@@ -3,6 +3,7 @@
 module.exports = {
   presets: [
     require.resolve('@babel/preset-env'),
+    require.resolve('@babel/preset-typescript'),
     require.resolve('@babel/preset-react'),
   ],
   plugins: [
