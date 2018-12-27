3164534cf396fa3bb7316e6812d1c3f0b237c7fb
Jamal Carvalho <jamal.carvalho@compass.com>
2018-12-27 01:26:12 -0500

    fix example tests

--- examples/service/test/quick-maths.test.js
+++ examples/service/test/quick-maths.test.js
@@ -1,9 +1,9 @@
 describe('Quick maths', () => {
   test('1 + 1 is 2', () => {
-    expect(1 + 1 === 2);
+    expect(1 + 1).toBe(2);
   });
 
   test('2 + 2 is 4', () => {
-    expect(2 + 2 === 2);
+    expect(2 + 2).toBe(4);
   });
 });
