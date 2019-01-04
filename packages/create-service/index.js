76830232bc15040ffc1fef6d8e5f56e81dfa9244
Jamal Carvalho <jamal.a.carvalho@gmail.com>
2019-01-03 22:53:26 -0500

    make directory argument optional in project initializers

--- packages/create-service/index.js
+++ packages/create-service/index.js
@@ -1,4 +1,4 @@
-import commander from 'commander';
+import program from 'commander';
 import chalk from 'chalk';
 import execa from 'execa';
 import fs from 'fs-extra';
@@ -6,64 +6,88 @@ import path from 'path';
 
 import packageJson from './package.json';
 
-let projectName;
-
-const program = new commander.Command(packageJson.name)
+program
+  .name(packageJson.name)
   .version(packageJson.version)
-  .arguments('<project-directory>')
-  .usage(`${chalk.green('<project-directory>')} [options]`)
-  .action(name => {
-    projectName = name;
-  })
-  .option('--typescript')
-  .allowUnknownOption()
-  .on('--help', () => {
-    console.log(chalk.green('halp'));
-  })
+  .arguments('[directory]')
+  .usage(`${chalk.green('[directory]')} [options]`)
+  .action(createProject)
+  .option('-t, --typescript', 'initialize using the typescript template')
+  .option('-f, --force', 'initialize in a non-empty directory')
   .parse(process.argv);
 
-if (fs.existsSync(projectName)) {
-  console.log(
-    chalk.yellow(`a directory with the name ${projectName} already exists`)
-  );
-  process.exit(1);
+function createProject(directory, cmd) {
+  let projectDir = process.cwd();
+  if (directory) {
+    fs.ensureDirSync(directory);
+    projectDir = path.resolve(directory);
+  }
+
+  const files = fs.readdirSync(projectDir);
+  if (files.length && !cmd.force) {
+    console.log(
+      `The directory ${path.resolve(
+        projectDir
+      )} already contains files.\n\nExiting.`
+    );
+    process.exit(1);
+  }
+
+  let existingPackageJson;
+  const packageJsonPath = path.resolve(projectDir, 'package.json');
+  if (fs.pathExistsSync(packageJsonPath)) {
+    existingPackageJson = fs.readJsonSync(packageJsonPath);
+  }
+
+  let templateDir = path.resolve(__dirname, 'templates/default');
+  if (cmd.typescript) {
+    templateDir = path.resolve(__dirname, 'templates/typescript');
+  }
+  fs.copySync(templateDir, projectDir);
+
+  if (existingPackageJson) {
+    const packageJson = fs.readJsonSync(packageJsonPath);
+    fs.outputJsonSync(
+      packageJsonPath,
+      {
+        ...existingPackageJson,
+        scripts: packageJson.scripts,
+        main: packageJson.main,
+        jest: packageJson.jest,
+      },
+      { spaces: 2 }
+    );
+  }
+
+  const dependencies = ['apollo-server', 'graphql'];
+  const devDependencies = ['@vaporweb/service'];
+  if (program.typescript) {
+    dependencies.push('ts-node', 'typescript');
+    devDependencies.push('@types/node', '@types/jest', '@types/graphql');
+  } else {
+    dependencies.push('esm');
+  }
+
+  process.chdir(projectDir);
+  console.log('installing dependencies...');
+  execa('npm', ['install', '--save', '--save-exact', ...dependencies])
+    .then(() => {
+      console.log('installed dependencies.');
+      console.log('installing dev dependencies...');
+      execa('npm', [
+        'install',
+        '--save-dev',
+        '--save-exact',
+        ...devDependencies,
+      ])
+        .then(() => console.log('success'))
+        .catch(err => {
+          console.log('npm install failed');
+          console.error(err);
+        });
+    })
+    .catch(err => {
+      console.log('npm install failed');
+      console.error(err);
+    });
 }
-
-fs.ensureDirSync(projectName);
-
-const root = path.resolve(projectName);
-
-let templateDir = path.resolve(__dirname, 'templates/default');
-if (program.typescript) {
-  templateDir = path.resolve(__dirname, 'templates/typescript');
-}
-
-fs.copySync(templateDir, root);
-
-const dependencies = ['apollo-server', 'graphql'];
-const devDependencies = ['@vaporweb/service'];
-if (program.typescript) {
-  dependencies.push('ts-node', 'typescript');
-  devDependencies.push('@types/node', '@types/jest', '@types/graphql');
-} else {
-  dependencies.push('esm');
-}
-
-process.chdir(root);
-
-console.log('installing dependencies...');
-execa('npm', ['install', '--save', '--save-exact', ...dependencies])
-  .then(() => {
-    console.log('installed dependencies.');
-    console.log('installing dev dependencies...');
-    execa('npm', ['install', '--save-dev', '--save-exact', ...devDependencies])
-      .then(() => console.log('success'))
-      .catch(err => {
-        console.log('npm install failed');
-        console.error(err);
-      });
-  })
-  .catch(err => {
-    console.log('npm install failed');
-    console.error(err);
-  });
